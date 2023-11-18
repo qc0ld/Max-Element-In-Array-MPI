73090c0e6534b931480955e343049cd0d328ea5d
qc0ld <then7w@yandex.ru>
2023-11-18 13:31:23 +0300

    Добавил замер времени общения между потоками

--- main.c
+++ main.c
@@ -4,58 +4,65 @@
 
 int main(int argc, char** argv)
 {
-	int ret  = -1;
-	int size = -1;
-	int rank = -1;
+    int ret  = -1;
+    int size = -1;
+    int rank = -1;
 
-	const int count = 100000000;
-	const int random_seed = 920215;
+    const int count = 100000000;
+    const int random_seed = 920215;
 
-	int* array = NULL; 
-	int lmax = -1;
-	int  max = -1; 
+    int* array = NULL; 
+    int lmax = -1;
+    int  max = -1; 
 
-	ret = MPI_Init(&argc, &argv);
+    ret = MPI_Init(&argc, &argv);
+    
+    if (!rank) { printf("MPI Init returned (%d);\n", ret); }
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    array = malloc(count * sizeof(int));
+
+    if (!rank) {
+        srand(random_seed);
+        for (int i = 0; i < count; i++) { array[i] = rand(); }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    double start_comm_time = MPI_Wtime();
+
+    MPI_Bcast(array, count, MPI_INTEGER, 0, MPI_COMM_WORLD);
 	
-	if (!rank) { printf("MPI Init returned (%d);\n", ret); }
+    double end_comm_time = MPI_Wtime();
 
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const int wstart = (rank) * count / size;
+    const int wend = (rank + 1) * count / size;
 
-	array = malloc(count * sizeof(int));
+    MPI_Barrier(MPI_COMM_WORLD);
 
-	if (!rank) {
-		srand(random_seed);
-		for (int i = 0; i < count; i++) { array[i] = rand(); }
-	}
+    double start_time = MPI_Wtime();
+    
+    for (int i = wstart; i < wend; i++)
+    {
+        if (array[i] > lmax) { lmax = array[i]; }
+    }
 
-	MPI_Bcast(array, count, MPI_INTEGER, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&lmax, &max, 1, MPI_INTEGER, MPI_MAX, 0, MPI_COMM_WORLD);
 
-	const int wstart = (rank) * count / size;
-	const int wend = (rank + 1) * count / size;
+    MPI_Barrier(MPI_COMM_WORLD);
 
-	MPI_Barrier(MPI_COMM_WORLD);
+    double end_time = MPI_Wtime();
 
-	double start_time = MPI_Wtime();
-	
-	for (int i = wstart; i < wend;i++)
-	{
-		if (array[i] > lmax) { lmax = array[i]; }
-	}
+    if (!rank) { 
+        printf("Execution time: %f seconds\n", end_time - start_time);
+        printf("Communication time: %f seconds\n", end_comm_time - start_comm_time);
+    }
 
-	MPI_Reduce(&lmax, &max, 1, MPI_INTEGER, MPI_MAX, 0, MPI_COMM_WORLD);
+    free(array);
 
-	MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
 
-	double end_time = MPI_Wtime();
-
-	if (!rank) { 
-		printf("Execution time: %f seconds\n", end_time - start_time);
-	}
-
-	free(array);
-
-	MPI_Finalize();
-
-	return(0);
+    return 0;
 }
